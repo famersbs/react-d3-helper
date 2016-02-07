2078d30b449ad691333e0e7c248f980a440a2e0b
서병선 <famersbs@gmail.com>
2016-02-07 13:44:39 -0800

    modify for work in real project

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-export { D3Shape } from './lib/type_1/D3Shape';
-export { D3Layer } from './lib/type_1/D3Layer';
-export { D3Element } from './lib/type_2/D3Element';
-export { D3ReactComponent } from './lib/D3ReactComponent';
+module.exports = {};
+
+module.exports.D3Shape = require( './lib/type_1/D3Shape' ).D3Shape;
+module.exports.D3Layer = require( './lib/type_1/D3Layer' ).D3Layer;
+module.exports.D3Element = require( './lib/type_2/D3Element' ).D3Element;
+module.exports.D3ReactComponent = require( './lib/D3ReactComponent' ).D3ReactComponent;
